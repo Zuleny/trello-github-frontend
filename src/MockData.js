4bcce581611c5f69e174a525114f4c952a810ee7
Zuleny Cruz <zuleny.cr@gmail.com>
2021-06-26 17:07:44 -0400

    [TRE-6,DOING] creating mockData

---
+++ src/MockData.js
@@ -0,0 +1,19 @@
+const mockData = {
+    lists: {
+        "01list": {
+            id: "01list",
+            title: "Bienvenido a Mi TrelloGit",
+            cards: [
+                {
+                    id: "01card",
+                    title: "Presiona Refresh ara actualizar los avances de tu repositorio Git"
+                }
+            ]
+        }
+    },
+    listIds: [
+        "01list"
+    ]
+};
+
+export default mockData;
